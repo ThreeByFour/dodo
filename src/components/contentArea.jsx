7a60e5a7eedbf603c15d46aebcad7c2aed3f7217
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-11-18 12:45:32 +0000

    Font + Itch

--- src/components/contentArea.jsx
+++ src/components/contentArea.jsx
@@ -5,12 +5,13 @@ const ContentArea = () => {
   return (
     <div className="content-area">
       <div className="logo-space">
-        {/* Logo will go here. You can replace it with an <img> tag */}
+        <img className="text-logo" src="assets/images/text-logo.png" alt="Dodo text logo" />
       </div>
       <div className="text-content">
         <p>Dodo is our upcoming puzzle game.</p>
         <p>Try our Itch demo</p>
-        <p>Check us out on Steam!</p>
+        {/* <p>Check us out on Steam!</p> */}
+        <iframe frameborder="0" src="https://itch.io/embed/2320056?border_width=3&amp;bg_color=ffffff&amp;link_color=3aceea" width="212" height="171"><a href="https://ollieboyne.itch.io/dodo-demo">Dodo demo by OllieBoyne</a></iframe>
       </div>
     </div>
   );
