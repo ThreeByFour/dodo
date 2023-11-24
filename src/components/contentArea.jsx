b3ff515879192c388f805be23eaf9de747eb85eb
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-11-24 18:56:47 +0000

    Itch url fix

--- src/components/contentArea.jsx
+++ src/components/contentArea.jsx
@@ -9,7 +9,7 @@ const ContentArea = () => {
       </div>
       <div className="text-content">
         <p>Dodo is our upcoming puzzle game.</p>
-        <p>Try out our web demo on <a href="ollieboyne.itch.io/dodo-demo"><b>itch.io</b></a>, and check us out on <a href="https://store.steampowered.com/app/2700250/Dodo/"><b>Steam</b></a></p>
+        <p>Try out our web demo on <a href="https://ollieboyne.itch.io/dodo-demo"><b>itch.io</b></a>, and check us out on <a href="https://store.steampowered.com/app/2700250/Dodo/"><b>Steam</b></a></p>
       </div>
       <div className="embed-container">
           <iframe frameborder="0" src="https://itch.io/embed/2320056?bg_color=ffffff&amp;fg_color=222222&amp;link_color=3aceea&amp;border_color=ffffff" width="100%" height="167"><a href="https://ollieboyne.itch.io/dodo-demo">Dodo demo by OllieBoyne</a></iframe>
